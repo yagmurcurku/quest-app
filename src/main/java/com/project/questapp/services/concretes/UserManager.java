4dd856bc5a76e76b8ada104335762110f648286c
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-23 14:18:36 +0300

    updated user manager

--- src/main/java/com/project/questapp/services/concretes/UserManager.java
+++ src/main/java/com/project/questapp/services/concretes/UserManager.java
@@ -1,8 +1,11 @@
 package com.project.questapp.services.concretes;
 
+import com.project.questapp.core.utilities.mappers.ModelMapperService;
 import com.project.questapp.entities.User;
 import com.project.questapp.repos.UserRepository;
 import com.project.questapp.services.abstracts.UserService;
+import com.project.questapp.services.dtos.requests.User.UserCreateRequest;
+import com.project.questapp.services.dtos.requests.User.UserUpdateRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ import java.util.List;
 public class UserManager implements UserService {
 
     private UserRepository userRepository;
+    private ModelMapperService modelMapperService;
 
     @Override
     public List<User> getAllUsers() {
@@ -25,17 +29,17 @@ public class UserManager implements UserService {
     }
 
     @Override
-    public User addUser(User user) {
-        return userRepository.save(user);
+    public void addUser(UserCreateRequest userCreateRequest) {
+        User user = modelMapperService.forRequest().map(userCreateRequest, User.class);
+        userRepository.save(user);
     }
 
     @Override
-    public User updateUser(User user) {
-        User userToUpdate = userRepository.findById(user.getId()).orElseThrow();
-        userToUpdate.setUserName(user.getUserName());
-        userToUpdate.setPassword(user.getPassword());
+    public void updateUser(UserUpdateRequest userUpdateRequest) {
+        User userToUpdate = userRepository.findById(userUpdateRequest.getId()).orElseThrow();
+        userToUpdate.setUsername(userUpdateRequest.getUsername());
+        userToUpdate.setPassword(userUpdateRequest.getPassword());
         userRepository.save(userToUpdate);
-        return userToUpdate;
     }
 
     @Override
