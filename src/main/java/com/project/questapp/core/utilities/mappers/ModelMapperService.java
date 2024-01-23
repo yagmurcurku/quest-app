103f72ce1628fcc62f014dd4d847bbef07b6ed85
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-23 14:13:03 +0300

    created modelmapper

---
+++ src/main/java/com/project/questapp/core/utilities/mappers/ModelMapperService.java
@@ -0,0 +1,9 @@
+package com.project.questapp.core.utilities.mappers;
+import org.modelmapper.ModelMapper;
+
+public interface ModelMapperService {
+
+    ModelMapper forResponse();
+    ModelMapper forRequest();
+
+}
