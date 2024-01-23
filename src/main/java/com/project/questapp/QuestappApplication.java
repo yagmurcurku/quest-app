8df38d08bbec66c11ecdd02b54b153df7236d337
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-23 14:21:08 +0300

    added getModelMapper Bean to QuestappApplication

--- src/main/java/com/project/questapp/QuestappApplication.java
+++ src/main/java/com/project/questapp/QuestappApplication.java
@@ -2,12 +2,21 @@ package com.project.questapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 public class QuestappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestappApplication.class, args);
+
 	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
+
 
 }
