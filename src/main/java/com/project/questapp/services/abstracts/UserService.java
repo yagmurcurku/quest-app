d3fb08d196b5e63af708394a7e4cb3bfaa9d1299
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-23 14:18:21 +0300

    updated user service

--- src/main/java/com/project/questapp/services/abstracts/UserService.java
+++ src/main/java/com/project/questapp/services/abstracts/UserService.java
@@ -1,6 +1,8 @@
 package com.project.questapp.services.abstracts;
 
 import com.project.questapp.entities.User;
+import com.project.questapp.services.dtos.requests.User.UserCreateRequest;
+import com.project.questapp.services.dtos.requests.User.UserUpdateRequest;
 
 import java.util.List;
 
@@ -8,8 +10,8 @@ public interface UserService {
 
     List<User> getAllUsers();
     User getUserById(Long userId);
-    User addUser(User user);
-    User updateUser(User user);
+    void addUser(UserCreateRequest userCreateRequest);
+    void updateUser(UserUpdateRequest userUpdateRequest);
     void deleteUser(Long userId);
 
 }
