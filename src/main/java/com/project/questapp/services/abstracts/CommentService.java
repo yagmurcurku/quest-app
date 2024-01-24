2112acaa99b823fc092dc196952ca9f63262fd39
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-24 16:24:29 +0300

    created comment service

--- src/main/java/com/project/questapp/services/abstracts/CommentService.java
+++ src/main/java/com/project/questapp/services/abstracts/CommentService.java
@@ -1,4 +1,17 @@
 package com.project.questapp.services.abstracts;
 
+import com.project.questapp.entities.Comment;
+import com.project.questapp.services.dtos.requests.Comment.CommentCreateRequest;
+import com.project.questapp.services.dtos.requests.Comment.CommentUpdateRequest;
+
+import java.util.List;
+
 public interface CommentService {
+
+    List<Comment> getAllComments();
+    Comment getCommentById();
+    void addComment(CommentCreateRequest commentCreateRequest);
+    void updateComment(CommentUpdateRequest commentUpdateRequest);
+    void deleteComment(Long id);
+
 }
