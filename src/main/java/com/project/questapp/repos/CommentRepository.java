fbacc5c965756ef799843ede3e833b190ee7e9d3
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-25 01:51:52 +0300

    created findByUserId and findByPostId

--- src/main/java/com/project/questapp/repos/CommentRepository.java
+++ src/main/java/com/project/questapp/repos/CommentRepository.java
@@ -1,8 +1,15 @@
 package com.project.questapp.repos;
 
 import com.project.questapp.entities.Comment;
+import com.project.questapp.entities.Post;
+import com.project.questapp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByUserId(Long userId);
+    List<Comment> findByPostId(Long postId);
 
 }
